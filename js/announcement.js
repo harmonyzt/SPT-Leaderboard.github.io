bd8eb94925b8700a57f6ceef88cb12196e1a4742
John Doe <hattrix@yandex.ru>
2025-05-24 00:20:42 +0500

    Update JS (minor)

--- js/announcement.js
+++ js/announcement.js
@@ -1,11 +1,10 @@
-//   _____ ____  ______   __    _________    ____  __________  ____  ____  ___    ____  ____ 
-//  / ___// __ \/_  __/  / /   / ____/   |  / __ \/ ____/ __ \/ __ )/ __ \/   |  / __ \/ __ \
-//  \__ \/ /_/ / / /    / /   / __/ / /| | / / / / __/ / /_/ / __  / / / / /| | / /_/ / / / /  
-// ___/ / ____/ / /    / /___/ /___/ ___ |/ /_/ / /___/ _, _/ /_/ / /_/ / ___ |/ _, _/ /_/ / 
-///____/_/     /_/    /_____/_____/_/  |_/_____/_____/_/ |_/_____/\____/_/  |_/_/ |_/_____/  
+//     _____ ____  ______   __    _________    ____  __________  ____  ____  ___    ____  ____ 
+//    / ___// __ \/_  __/  / /   / ____/   |  / __ \/ ____/ __ \/ __ )/ __ \/   |  / __ \/ __ \
+//    \__ \/ /_/ / / /    / /   / __/ / /| | / / / / __/ / /_/ / __  / / / / /| | / /_/ / / / /  
+//   ___/ / ____/ / /    / /___/ /___/ ___ |/ /_/ / /___/ _, _/ /_/ / /_/ / ___ |/ _, _/ /_/ / 
+//  /____/_/     /_/    /_____/_____/_/  |_/_____/_____/_/ |_/_____/\____/_/  |_/_/ |_/_____/  
 
 document.addEventListener('DOMContentLoaded', function () {
-
     const closeButton = document.querySelector('.close-notification');
     const notification = document.querySelector('.leaderboard-notification');
 
@@ -14,8 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
             closeNotification();
         });
 
-        // 50 second timeout
-        const autoCloseTimer = setTimeout(closeNotification, 50000);
+        // 30 second timeout
+        const autoCloseTimer = setTimeout(closeNotification, 30000);
 
         function closeNotification() {
             clearTimeout(autoCloseTimer);
