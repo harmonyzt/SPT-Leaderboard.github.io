3ffc5a044b387c25e7a7f8868a13fec4375fcd0f
John Doe <hattrix@yandex.ru>
2025-05-31 04:09:31 +0500

    1

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ let oldTotalPlayTime = 0;
 
 // https://visuals.nullcore.net/hidden/seasons/season
 // fallbacks/season [DEBUG]
-const seasonPath = "fallbacks/season";
+const seasonPath = "https://visuals.nullcore.net/hidden/seasons/season";
 const seasonPathEnd = ".json";
 
 /**
