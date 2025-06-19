75380487368cb94b4907a8fbee4afaf52923b35d
John Doe <hattrix@yandex.ru>
2025-06-19 10:53:52 +0500

    Remove announecementanmnatant

--- js/announcement.js
+++ js/announcement.js
@@ -4,26 +4,3 @@
 //   ___/ / ____/ / /    / /___/ /___/ ___ |/ /_/ / /___/ _, _/ /_/ / /_/ / ___ |/ _, _/ /_/ / 
 //  /____/_/     /_/    /_____/_____/_/  |_/_____/_____/_/ |_/_____/\____/_/  |_/_/ |_/_____/  
 
-document.addEventListener('DOMContentLoaded', function () {
-    const closeButton = document.querySelector('.close-notification');
-    const notification = document.querySelector('.leaderboard-notification');
-
-    if (closeButton && notification) {
-        closeButton.addEventListener('click', function () {
-            closeNotification();
-        });
-
-        // 12 second timeout
-        const autoCloseTimer = setTimeout(closeNotification, 12000);
-
-        function closeNotification() {
-            clearTimeout(autoCloseTimer);
-            notification.classList.add('hide');
-
-            notification.addEventListener('animationend', function handler() {
-                notification.removeEventListener('animationend', handler);
-                notification.remove();
-            });
-        }
-    }
-});
