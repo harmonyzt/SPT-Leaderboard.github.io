4f4d4461292f51bfb82a93648a3bedb4926c7210
John Doe <hattrix@yandex.ru>
2025-07-23 23:33:31 +0500

    Fix online count

--- js/heartbeatMonitor.js
+++ js/heartbeatMonitor.js
@@ -8,7 +8,7 @@ class HeartbeatMonitor {
     constructor() {
         this.heartbeatData = {};
         this.lastUpdateTime = 0;
-        this.onlineThreshold = 1200;
+        this.onlineThreshold = 1800;
     }
 
     async fetchHeartbeats() {
